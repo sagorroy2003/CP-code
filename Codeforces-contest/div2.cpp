c5f41fa6ffedff5550c79c7fe6d130fe134e7767
Sagor Roy <sagor2003roy@gmail.com>
2025-08-27 13:09:07 +0600

    Create new folder

---
+++ Codeforces-contest/div2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Your code goes here
+    int n; cin >> n; 
+    cout << n << endl;
+
+    return 0;
+}
