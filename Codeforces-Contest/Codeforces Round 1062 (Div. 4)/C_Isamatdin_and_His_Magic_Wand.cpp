657783d11e092f14f25e92d57e0287543b9466da
Sagor Roy <sagor2003roy@gmail.com>
2025-11-05 20:25:19 +0600

    new start in CP

---
+++ Codeforces-Contest/Codeforces Round 1062 (Div. 4)/C_Isamatdin_and_His_Magic_Wand.cpp
@@ -0,0 +1,113 @@
+// ------------------------------------------------------------------
+//            Author: SAGOR ROY
+//            Date: 05 November 2025 (Wednesday) | 18:42
+// ------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Aliases ---
+using ll = long long;
+using lld = long double;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// --- Constants ---
+const lld pi = acos(-1.0);
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+// --- Macros ---
+#define en '\n'
+#define endl '\n'
+#define yes cout << "YES" << en
+#define no cout << "NO" << en
+
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define pf push_front
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / __gcd(a, b) * b)
+
+#define input(v)       \
+    for (auto &it : v) \
+        cin >> it;
+#define output(v)      \
+    for (auto &it : v) \
+        cout << it << " ";
+
+// --- Utility Functions ---
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// ------------------------------------------------------------------
+// ------------------- S O L V E -------------------
+// ------------------------------------------------------------------
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi a(n);
+
+    int e = 0, o = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+
+        if (a[i] % 2)
+        {
+            o++;
+        }
+        else
+        {
+            e++;
+        }
+    }
+
+    if (e > 0 && o > 0)
+    {
+        srt(a);
+        output(a);
+        cout << en;
+    }
+    else
+        output(a);
+    cout << en;
+}
+
+// ------------------------------------------------------------------
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    // #ifndef ONLINE_JUDGE
+    // 	freopen("D:/CP/input.txt", "r", stdin);
+    // 	freopen("D:/CP/output.txt", "w", stdout);
+    // #endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
