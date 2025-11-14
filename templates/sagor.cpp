5f58b7c7a4850dd9080789567b019aacb7d410c0
Sagor Roy <sagor2003roy@gmail.com>
2025-11-14 20:10:00 +0600

    update template

--- templates/sagor.cpp
+++ templates/sagor.cpp
@@ -1,76 +1,77 @@
-///                                   SAGOR ROY 2.0
+// ------------------------------------------------------------------
+//            Author: SAGOR ROY
+//            Date: 14 November 2025 (Friday) | 20:09
+// ------------------------------------------------------------------
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#define en endl
-#define pi acos(-1.00)
-#define ll long long
-#define lld long double
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * b) / gcd(a, b)
-// ll lcm(ll a, ll b)
-// {
-// 	return a / __gcd(a, b) * b;
-// }
+// --- Type Aliases ---
+using ll = long long;
+using lld = long double;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<ll>;
 
-#define yes cout << "YES" << '\n'
-#define no cout << "NO" << '\n'
-#define srt(v) sort(v.begin(), v.end());
-#define rsrt(v) sort(v.rbegin(), v.rend());
-#define rev(v) reverse(v.begin(), v.end());
+// --- Constants ---
+const lld pi = acos(-1.0);
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+// --- Macros ---
+#define en '\n'
+#define endl '\n'
+#define yes cout << "YES" << en
+#define no cout << "NO" << en
+
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
 #define all(x) x.begin(), x.end()
-
-#define input(v)    \
-    for (auto &i : v) \
-        cin >> i;
-#define output(v)   \
-    for (auto &i : v) \
-        cout << i << " ";
-
 #define pb push_back
 #define pf push_front
-#define pii pair<int, int>
-#define vi vector<int>
-#define vll vector<ll>
-#define vlld vector<lld>
 
-bool isPrime(int n)
-{
-    if (n < 2)
-        return false;
-    for (int i = 2; i * i <= n; i++)
-    {
-        if (n % i == 0)
-            return false;
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / __gcd(a, b) * b)
+
+#define input(v) \
+    for (auto &it : v) \
+        cin >> it;
+#define output(v) \
+    for (auto &it : v) \
+        cout << it << " ";
+
+// --- Utility Functions ---
+bool isPrime(int n) {
+    if (n < 2) return false;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
     }
     return true;
 }
 
-int dx[4] = {-1, 1, -1, 1}, dy[4] = {-1, -1, 1, 1};
+// ------------------------------------------------------------------
+// ------------------- S O L V E -------------------
+// ------------------------------------------------------------------
 
-const int MOD = 1e9 + 7;
-const int MAX = 1e5 + 5;
-
-void solve_A()
-{
-    // code here...
+void solve() {
+    
 }
 
-int main()
-{
+// ------------------------------------------------------------------
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(0), cout.tie(0);
 
-// #ifndef ONLINE_JUDGE
-// 	freopen("D:/CP/input.txt", "r", stdin);
-// 	freopen("D:/CP/output.txt", "w", stdout);
-// #endif
+    // #ifndef ONLINE_JUDGE
+    // 	freopen("D:/CP/input.txt", "r", stdin);
+    // 	freopen("D:/CP/output.txt", "w", stdout);
+    // #endif
 
-    int t;
+    int t = 1;
     cin >> t;
-    while (t--)
-    {
-        solve_A();
+    while (t--) {
+        solve();
     }
 
     return 0;
