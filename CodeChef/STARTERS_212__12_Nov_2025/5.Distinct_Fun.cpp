0f24567a61b50825f373a5def0371e4fa709b7e7
Sagor Roy <sagor2003roy@gmail.com>
2025-11-14 20:09:13 +0600

    upsolve question 5

---
+++ CodeChef/STARTERS_212__12_Nov_2025/5.Distinct_Fun.cpp
@@ -0,0 +1,110 @@
+// ------------------------------------------------------------------
+//            Author: SAGOR ROY
+//            Date: 14 November 2025 (Friday) | 17:03
+// ------------------------------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Type Aliases ---
+using ll = long long;
+using lld = long double;
+using pii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+
+// --- Constants ---
+const lld pi = acos(-1.0);
+const int MOD = 1e9 + 7;
+const int MAX = 1e5 + 5;
+
+// --- Macros ---
+#define en '\n'
+#define endl '\n'
+#define yes cout << "YES" << en
+#define no cout << "NO" << en
+
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define rev(v) reverse(v.begin(), v.end())
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define pf push_front
+
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / __gcd(a, b) * b)
+
+#define input(v)       \
+    for (auto &it : v) \
+        cin >> it;
+#define output(v)      \
+    for (auto &it : v) \
+        cout << it << " ";
+
+// --- Utility Functions ---
+bool isPrime(int n)
+{
+    if (n < 2)
+        return false;
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+// ------------------------------------------------------------------
+// ------------------- S O L V E -------------------
+// ------------------------------------------------------------------
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vi v(n);
+    input(v);
+
+    int ans = 0;
+
+    unordered_set<int> a;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        auto it = a.find(v[i]);
+
+        if (it != a.end()) // that means we find duplicate 
+        {                  // we have to do X-OR to the remaining half 
+            ans++;
+            a.clear();// we clear this part bcuz this part is distinct 
+            a.insert(v[i]);
+        }
+        else
+        {
+            a.insert(v[i]);
+        }
+    }
+
+    cout << ans << en;
+}
+
+// ------------------------------------------------------------------
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0), cout.tie(0);
+
+    // #ifndef ONLINE_JUDGE
+    // 	freopen("D:/CP/input.txt", "r", stdin);
+    // 	freopen("D:/CP/output.txt", "w", stdout);
+    // #endif
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
